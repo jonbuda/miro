836ff558a9149f84e5833e5b4bb2362f1e9a8469
Ryan LeFevre <meltingice8917@gmail.com>
2013-07-18 13:44:43 -0400

    Allow manually specifying image type to avoid Tempfile issues

--- lib/miro/dominant_colors.rb
+++ lib/miro/dominant_colors.rb
@@ -2,8 +2,9 @@ module Miro
   class DominantColors
     attr_accessor :src_image_path
 
-    def initialize(src_image_path)
+    def initialize(src_image_path, image_type = nil)
       @src_image_path = src_image_path
+      @image_type = image_type
     end
 
     def to_hex
@@ -54,7 +55,7 @@ module Miro
 
     def open_source_image
       if remote_source_image?
-        original_extension = URI.parse(@src_image_path).path.split('.').last
+        original_extension = @image_type || URI.parse(@src_image_path).path.split('.').last
 
         tempfile = Tempfile.open(["source", ".#{original_extension}"])
         remote_file_data = open(@src_image_path).read
